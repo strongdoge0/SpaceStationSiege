4800c082376eb42bdbcf74051cf888dcd58d4159
strongdoge0 <126104153+strongdoge0@users.noreply.github.com>
2025-06-03 14:19:57 +0600

    update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,9 +6,17 @@ public class CameraController : MonoBehaviour
     public Vector3 offset = new Vector3(0, 2.2f, -6);
     public UIController uIController;
     private Unit _targetInCrosshair = null;
+
+
+    public float normalFOV = 75;
+    public float zoomedFOV = 35;
+    private float _currentFOV = 75;
+    public float zoomSpeed = 5.0f;
+
+
     void Start()
     {
-        
+
     }
 
 
@@ -21,13 +29,28 @@ public class CameraController : MonoBehaviour
 
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
-            if (Physics.Raycast(ray,out hit))
+            if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider.GetComponent<Unit>()) {
+                if (hit.collider.GetComponent<Unit>())
+                {
                     _targetInCrosshair = hit.collider.GetComponent<Unit>();
                 }
             }
             uIController.DrawTargetStatusBar(_targetInCrosshair);
+
+            if (Input.GetMouseButton(1))
+            {
+                _currentFOV = Mathf.Lerp(_currentFOV, zoomedFOV, Time.deltaTime * zoomSpeed);
+            }
+            else
+            {
+                _currentFOV = Mathf.Lerp(_currentFOV, normalFOV, Time.deltaTime * zoomSpeed);
+            }
+            Camera.main.fieldOfView = _currentFOV;
+        }
+        else
+        {
+            _currentFOV = normalFOV;
         }
     }
 }
